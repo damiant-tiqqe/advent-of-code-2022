17d44cc0b85a26b81f709de3fadceeaef307b03b
Damian Thornton <damian.thornton@postnord.com>
2022-12-06 12:15:09 +0100

    day 6 part 2 added dynamic regex function added also filthy 2 line condensed versions added to be disgusting

--- src/day6.ts
+++ src/day6.ts
@@ -9,17 +9,52 @@ export const run = () => {
 
     console.log(`Part 1: First start of packet marker location: ${markerPosition}`);
 
-    console.log(`Part 2: UNKNOWN`);
+    const markerPosition2 = getNonRepeatingPattern(dataRaw[0], 14);
+
+    console.log(`Part 2: First start of message marker location: ${markerPosition2}`);
 
 }
 
-export function getNonRepeatingPattern(input: string): number {
-    const regex: RegExp = /(\w)(?!\1)(\w)(?!\1|\2)(\w)(?!\1|\2|\3)(\w)(?!\1|\2|\3|\4)/;
+/**
+ * because sometimes it needs to be filthy
+ * @returns 
+ */
+export const part1 = () => {
+    const match = (new RegExp(/(\w)(?!\1)(\w)(?!\1|\2)(\w)(?!\1|\2|\3)\w/)).exec(Util.readDataFile('data/day6/input.txt')[0]);
+    return match && match[0].length === 4 ? match[0].length + match['index'] : -1;
+}
+
+/**
+ * because sometimes it needs to be filthy
+ * @returns 
+ */
+export const part2 = () => {
+    const match = (new RegExp(/(\w)(?!\1)(\w)(?!\1|\2)(\w)(?!\1|\2|\3)(\w)(?!\1|\2|\3|\4)(\w)(?!\1|\2|\3|\4|\5)(\w)(?!\1|\2|\3|\4|\5|\6)(\w)(?!\1|\2|\3|\4|\5|\6|\7)(\w)(?!\1|\2|\3|\4|\5|\6|\7|\8)(\w)(?!\1|\2|\3|\4|\5|\6|\7|\8|\9)(\w)(?!\1|\2|\3|\4|\5|\6|\7|\8|\9|\10)(\w)(?!\1|\2|\3|\4|\5|\6|\7|\8|\9|\10|\11)(\w)(?!\1|\2|\3|\4|\5|\6|\7|\8|\9|\10|\11|\12)(\w)(?!\1|\2|\3|\4|\5|\6|\7|\8|\9|\10|\11|\12|\13)\w/)).exec(Util.readDataFile('data/day6/input.txt')[0]);
+    return match && match[0].length === 14 ? match[0].length + match['index'] : -1;
+}
+
+export function getNonRepeatingPattern(input: string, patternLength: number = 4): number {
+    //const regex: RegExp = /(\w)(?!\1)(\w)(?!\1|\2)(\w)(?!\1|\2|\3)\w/;
+    const pattern: string = getDynamicRegex(patternLength)
+    const regex: RegExp = new RegExp(pattern)
     const match = regex.exec(input) || [];
 
-    if (match && match[0].length === 4) {
+    if (match && match[0].length === patternLength) {
         return match[0].length + match['index'];
     }
 
     return -1;
+}
+
+export const getDynamicRegex = (length: number): string => {
+    const result: string[] = [];
+    for (let i = 1; i < length; i++) {
+        const sections: string[] = [];
+        for (let j = 1; j <= i; j++) {
+            sections.push(`\\${j}`)
+        }
+        result.push(`(\\w)(?!${sections.join('|')})`);      
+    }
+
+    return result.join('') + '\\w';
 }
--- src/days6..spec.ts
+++ src/days6..spec.ts
@@ -44,4 +44,54 @@ describe(`Advent of Code - Day 6`, async () => {
         const result = Day6.getNonRepeatingPattern(input);
         assert.equal(result, expected);
     });
+
+
+    it(`should find marker at position 19`, async () => {
+        const input: string = 'mjqjpqmgbljsphdztnvjfqwrcgsmlb';
+        const expected = 19;
+    
+        const result = Day6.getNonRepeatingPattern(input, 14);
+        assert.equal(result, expected);
+    });
+
+    it(`should find marker at position 23`, async () => {
+        const input: string = 'bvwbjplbgvbhsrlpgdmjqwftvncz';
+        const expected = 23;
+    
+        const result = Day6.getNonRepeatingPattern(input, 14);
+        assert.equal(result, expected);
+    });
+
+    
+    it(`should find marker at position 23`, async () => {
+        const input: string = 'nppdvjthqldpwncqszvftbrmjlhg';
+        const expected = 23;
+    
+        const result = Day6.getNonRepeatingPattern(input, 14);
+        assert.equal(result, expected);
+    });    
+
+    it(`should find marker at position 29`, async () => {
+        const input: string = 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg';
+        const expected = 29;
+    
+        const result = Day6.getNonRepeatingPattern(input, 14);
+        assert.equal(result, expected);
+    });
+
+    it(`should find marker at position 26`, async () => {
+        const input: string = 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw';
+        const expected = 26;
+    
+        const result = Day6.getNonRepeatingPattern(input, 14);
+        assert.equal(result, expected);
+    });
+
+    it(`should not be this filthy: length 4`, async () => {
+        assert.equal(Day6.part1(), 1757);
+    });
+
+    it(`should not be this filthy: length 14`, async () => {
+        assert.equal(Day6.part2(), 2950);
+    });
 });
