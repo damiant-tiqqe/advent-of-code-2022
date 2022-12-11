62fd1f1a23b40a24d743478b1c24db297e6b0b20
Damian Thornton <damian.thornton@postnord.com>
2022-12-11 08:52:35 +0100

    Initial setup of day11 files and first unit test for first logic definition in requirements

---
+++ src/day11.spec.ts
@@ -0,0 +1,26 @@
+import { assert } from 'chai';
+import {cloneDeep} from 'lodash';
+import * as Util from './util';
+import * as Day11 from './day11';
+
+describe(`Advent of Code 2022 - Day 11`, async () => {
+    
+    // place holder to test run without build
+    it.skip(`Day11.run()`, async () => {
+        Day11.run();
+    });
+
+
+    describe(`After each monkey inspects an item but before it tests your worry level, 
+    your relief that the monkey's inspection didn't damage the item causes 
+    your worry level to be divided by three and rounded down to the nearest integer.`, async () => {
+
+        it.skip(`reduce worry 7 equals 2`, async () => {
+            const worryLevel: number = 7;
+            const expected: number = 2;
+        
+            const result = Day11.reduceWorryLevel(worryLevel);
+            assert.equal(result, expected);
+        });
+    });
+});
---
+++ src/day11.ts
@@ -0,0 +1,15 @@
+import * as Util from './util';
+import {cloneDeep} from 'lodash';
+
+export const run = ():void => {
+    console.log(`----------- Day 11 -----------`);
+    const dataRaw = Util.readDataFile('data/day11/input.txt');
+
+    console.log(`Part 1: `);
+
+
+
+    console.log(`Part 2: `);
+
+
+};
